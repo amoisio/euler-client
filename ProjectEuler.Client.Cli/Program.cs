f11fb93a00da4a1194b886624d014ebcd574e2d1
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-08-22 23:18:30 +0300

    Working implementation of the OnlineAPI and file generator. Added Cli project.

---
+++ ProjectEuler.Client.Cli/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using CliFx;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using ProjectEuler.Client;
+using System.Collections.Generic;
+
+namespace ProjectEuler.Client.Cli
+{
+    class Program
+    {
+        static readonly HttpClient _httpClient = new HttpClient();
+        static async Task<int> Main(string[] args)
+        {
+            int problemNumber = ParseProblemNumber(args);
+            
+            var api = new OnlineAPI(_httpClient);
+            var details = await api.ProblemDetailsFor(problemNumber);
+
+            System.Console.WriteLine(details.Title);
+            System.Console.WriteLine(details.DetailsHtml);
+
+            var gen = new ProblemFileGenerator(details);
+            foreach (var (templatePath, outputPath) in ParseTemplatePaths(args)) 
+            {
+                await gen.GenerateAsync(templatePath, outputPath);
+            }
+            return 1;
+        }
+
+        private static int ParseProblemNumber(string[] args) => Int32.Parse(args[0]);
+
+        private static IEnumerable<(string, string)> ParseTemplatePaths(string[] args) 
+        {
+            int len = args.Length;
+            for(int i = 1; i < len; i += 2) 
+            {
+                string templatePath = args[i];
+                string outputPath = (args.Length > i + 1)
+                    ? args[i + 1]
+                    : ".";
+                yield return (templatePath, outputPath);
+            }
+            
+        }
+    }
+}
