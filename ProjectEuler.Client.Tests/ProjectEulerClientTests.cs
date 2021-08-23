308b5117779fb5bffb240a33d0870a2da802a1e2
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-08-23 17:41:57 +0300

    Add tests for template generation

--- ProjectEuler.Client.Tests/ProjectEulerClientTests.cs
+++ ProjectEuler.Client.Tests/ProjectEulerClientTests.cs
@@ -2,9 +2,72 @@ using Xunit;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ProjectEuler.Client.Tests
 {
+    public class Problem_Generator : IDisposable 
+    {
+        private readonly HttpClient _client;
+        private readonly OnlineAPI _api;
+        private bool disposedValue;
+
+        public Problem_Generator()
+        {
+            _client = new HttpClient();
+            _api = new OnlineAPI(_client);
+        }
+
+        [Fact]
+        public async Task Should_generate_the_output_file()
+        {
+            var details = await _api.ProblemDetailsFor(1);
+            var generator = new ProblemFileGenerator(details);
+            
+            var outputPath = $"./{Guid.NewGuid()}Output.txt";
+            await generator.GenerateAsync("./Template.txt", outputPath);
+
+            Assert.True(File.Exists(outputPath));
+        }
+
+        [Fact]
+        public async Task Should_generate_a_file_which_contains_templated_information()
+        {
+            var details = await _api.ProblemDetailsFor(1);
+            var generator = new ProblemFileGenerator(details);
+
+            var outputPath = $"./{Guid.NewGuid()}Output.txt";
+            await generator.GenerateAsync("./Template.txt", outputPath);
+
+            var contents = await File.ReadAllTextAsync(outputPath);
+
+            Assert.Contains(details.Title, contents);
+            Assert.Contains(details.Ref, contents);
+            Assert.Contains(details.Number.ToString(), contents);
+            Assert.Contains(details.DetailsHtml, contents);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _client.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+
     public class OnlineAPI_ProblemDetailsFor : IDisposable  
     {
         private readonly HttpClient _client;
